51e26e046242516fa3f639e00f5f94a3c73db4a0
horse37 <daniel@infopowersistemas.com.br>
2025-06-26 11:01:33 -0300

    Pronto - Ajuset Email Temporário

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -119,7 +119,7 @@ const Footer = () => {
               <div className="flex items-center space-x-3">
                 <Mail className="w-5 h-5 text-primary-400 flex-shrink-0" />
                 <span className="text-gray-400 text-sm">
-                  contato@imobiliariamoderna.com.br
+                  coopercorretores@gmail.com
                 </span>
               </div>
             </div>
